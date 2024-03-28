9f8029a7a30432cfd2ba8a32253797d2f7944c7d
NiTro005 <nitro2005002@gmail.com>
2024-03-28 10:26:28 +0300

    developed a method paint and rendering 2 objects

---
+++ TVP-projects/Laba-2/EV3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_2
+{
+    internal class EV3
+    {
+        int x, y;
+        int w, h;
+
+        public EV3(int x, int y, int w, int h)
+        {
+            this.x = x;
+            this.y = y;
+            this.w = w;
+            this.h = h;
+        }
+
+        internal void Paint(Graphics g)
+        {
+            g.DrawRectangle(Pens.Black, x, y, w, h);
+        }
+    }
+}
--- TVP-projects/Laba-2/Form1.cs
+++ TVP-projects/Laba-2/Form1.cs
@@ -12,9 +12,20 @@ namespace Laba_2
 {
     public partial class Form1 : Form
     {
+        EV3 l1, l2;
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            l1.Paint(g);
+            l2.Paint(g);
+        }
+
         public Form1()
         {
             InitializeComponent();
+            l1 = new EV3(100, 100, 100, 100);
+            l2 = new EV3(150, 100, 300, 150);
         }
     }
 }
