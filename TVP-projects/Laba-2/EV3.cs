af444526c137e46da498f0c0e03c2c399e395b75
NiTro005 <nitro2005002@gmail.com>
2024-03-28 12:07:48 +0300

    EV3 rendering implemented

--- TVP-projects/Laba-2/EV3.cs
+++ TVP-projects/Laba-2/EV3.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Laba_2
 {
@@ -11,6 +12,7 @@ namespace Laba_2
     {
         int x, y;
         int w, h;
+        ComboBox ComboBox1;
 
         public EV3(int x, int y, int w, int h)
         {
@@ -18,10 +20,22 @@ namespace Laba_2
             this.y = y;
             this.w = w;
             this.h = h;
+            ComboBox1 = new ComboBox();
+            ComboBox1.Left = x;
+            ComboBox1.Top = y;
+            ComboBox1.Width = w / 10;
         }
-
+        Bitmap bitmap = (Bitmap)Bitmap.FromFile("C:\\Sourse\\github\\TVP-Work\\TVP-projects\\Laba-2\\Image\\001.png");
         internal void Paint(Graphics g)
         {
+            if (h == 70)
+            {
+                g.DrawImage(bitmap, x, y, w, h);
+            }
+            if(h == 35)
+            {
+                g.FillRectangle(Brushes.DarkOrange, x, y, w, h);
+            }
             g.DrawRectangle(Pens.Black, x, y, w, h);
         }
     }
--- TVP-projects/Laba-2/Form1.cs
+++ TVP-projects/Laba-2/Form1.cs
@@ -12,20 +12,22 @@ namespace Laba_2
 {
     public partial class Form1 : Form
     {
-        EV3 l1, l2;
+        EV3 l1, l2, l3;
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = this.CreateGraphics();
             l1.Paint(g);
             l2.Paint(g);
+            l3.Paint(g);
         }
 
         public Form1()
         {
             InitializeComponent();
-            l1 = new EV3(100, 100, 100, 100);
+            l1 = new EV3(150, 100, 300, 35);
             l2 = new EV3(150, 100, 300, 150);
+            l3 = new EV3(150, 135, 300, 70);
         }
     }
 }
